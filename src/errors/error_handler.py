df131a21af78f2e0d78ba4c7b0e8d9542246db9b
Bruno Fukumori <brunofukumori@gmail.com>
2024-02-09 10:35:25 -0300

    feat: implementing validators and error handler

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,10 +1,18 @@
 from src.views.http_types.http_response import HttpResponse
+from src.errors.error_types.http_unprocessable_entity_error import HttpUnprocessableEntityError
 
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={"errors": [{
+                "name": error.name,
+                "detail": error.message
+            }]})
     return HttpResponse(
         status_code=500,
         body={"errors": [{
-            "title": "Internal Server Error",
+            "name": "Internal Server Error",
             "detail": str(error)
         }]})
